23eea32b89036ef91ee4e8e8a2bce643f5d45644
chshahid119 <shahidchaudhary0729@gmail.com>
2025-01-09 22:35:55 +0000

    Compiler Error and Compiler Warnings

---
+++ C++ NOTES/DR FRANK MITROPOLOUS COURSE NOTES/COURSE NOTES (OWN)/Section4/CompilerErrors/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // Those all are examples of compiler errors 
+    cout << ("Hello world"/25) << endll
+    
+    return 0;
+}
---
+++ C++ NOTES/DR FRANK MITROPOLOUS COURSE NOTES/COURSE NOTES (OWN)/Section4/CompilerWarnnings/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int favorite_number;
+    
+    favorite_number=10;
+    
+    cout << "Hello World" << endl;
+    
+    return 0;
+}
