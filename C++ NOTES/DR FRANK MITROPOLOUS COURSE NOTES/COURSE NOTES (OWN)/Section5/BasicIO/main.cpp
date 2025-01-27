5e4ecc7d980c76731310741b45ae47dc3e4edce2
chshahid119 <shahidchaudhary0729@gmail.com>
2025-01-27 23:02:25 +0000

    Basic Input and Output (I/O) using cin and cout

---
+++ C++ NOTES/DR FRANK MITROPOLOUS COURSE NOTES/COURSE NOTES (OWN)/Section5/BasicIO/main.cpp
@@ -0,0 +1,34 @@
+// Section 5
+// Basic I/O using cin and cout
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//	cout << "Hello World!" << endl;
+
+//  cout << "Hello";
+//  cout << "World" << endl;
+
+//    cout << "Hello World" << endl;
+//    cout << "Hello" << " World" << endl;
+//    cout << "Hello" << " World!\n";
+//    cout << "Hello\nOut\nThere\n";
+
+    int num1;
+    int num2;
+//    double num3;
+    
+    cout << "Enter an integer:";
+    cin >> num1;
+    cout << endl;
+    cout << "Enter second Integer:";
+    cin >> num2;
+    cout << endl;
+    cout << "You Entered: " << num1 << " and " << num2 << endl;
+
+    
+	return 0;
+}
