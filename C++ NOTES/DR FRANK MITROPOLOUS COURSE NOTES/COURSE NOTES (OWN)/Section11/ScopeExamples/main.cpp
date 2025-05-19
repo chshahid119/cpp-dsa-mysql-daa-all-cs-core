a1802071086f4c6569554ceb898ef78a77a05aae
chshahid119 <shahidchaudhary0729@gmail.com>
2025-05-19 03:49:12 +0100

    add two more function example for global and static variables

--- C++ NOTES/DR FRANK MITROPOLOUS COURSE NOTES/COURSE NOTES (OWN)/Section11/ScopeExamples/main.cpp
+++ C++ NOTES/DR FRANK MITROPOLOUS COURSE NOTES/COURSE NOTES (OWN)/Section11/ScopeExamples/main.cpp
@@ -2,11 +2,32 @@
 
 using namespace std;
 
+void local_example();
+void global_example();
+void static_local_example();
+
+int num {300}; // Global variable - declared outside any class or function
+
+void global_example(){
+    cout << "\nGlobal num is: " << num << " in global_example - start" << endl;
+    num *=2;
+    cout << "Global num is: " << num << " in global_example - end" << endl;
+}
+
 void local_example(int x){
     int num {1000}; // local to local_example
     cout << "\nlocal example is: " << num << " in local_example - start" << endl;
     num=x;
     cout << "Local num is: " << num << " in local example - end" << endl;
+    // num1 in main is not within scope  - so it can't be used here
+}
+
+void static_local_example(){
+    static int num {5000}; // local to static_local_example - retains its value between calls
+    cout << "\nLocal static num is: " << num << " in static_local_example - start" << endl;
+    num += 1000;
+    cout << "Local static num is: " << num << " in static_local_example - end" << endl;
+    
 }
 
 int main(){
@@ -25,5 +46,12 @@ int main(){
     cout << "Local num is: " << num << " in main" << endl;
     
     local_example(10);
+    local_example(20);
+    
+    global_example();
+    global_example();
+    
+    
+    cout << endl;
     return 0;
 }
