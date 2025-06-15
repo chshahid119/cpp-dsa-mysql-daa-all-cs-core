f06ebcc38ed330c7720a9264e6c7620a12da0345
chshahid119 <shahidchaudhary0729@gmail.com>
2025-06-15 15:29:39 +0100

    One Example from stack Overflow added in notes (understanding of Arrays and Pointers Relationships)

---
+++ C++ NOTES/DR FRANK MITROPOLOUS COURSE NOTES/COURSE NOTES (OWN)/Section12/ArrayVsPointersRelationship/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int vals[] = {1,50,3,28,32,500};
+    
+    int* valptr = vals;
+    
+    
+    cout << vals << endl;
+    cout << &vals << endl;
+    cout << *(&vals) << endl;
+    cout << *(*(&vals)) << endl; // Now it derefence to that address to give value
+    cout<<valptr<<endl;
+
+    cout << endl;
+    cout << endl;
+    
+    cout<<valptr[1]<<endl;//outputs 50
+    cout<<vals[1]<<endl;//outputs 50
+    cout<<*vals<<endl;//outputs 1
+    cout<<*valptr<<endl;//outputs 1
+    
+    cout << endl;
+    cout << endl;
+    
+    cout<<*(&valptr +1)-valptr<<endl;
+
+    cout<<endl;
+
+    cout<< *(&vals + 1) -vals<<endl;
+
+    return 0;
+}
