ee9b9e5fbc8d2684a856f5ac80d77f5a75340f42
chshahid119 <shahidchaudhary0729@gmail.com>
2025-06-25 13:41:02 +0100

    everything updated

--- C++ NOTES/DR FRANK MITROPOLOUS COURSE NOTES/COURSE NOTES (OWN)/Section12/ArrayVsPointersRelationship/main.cpp
+++ C++ NOTES/DR FRANK MITROPOLOUS COURSE NOTES/COURSE NOTES (OWN)/Section12/ArrayVsPointersRelationship/main.cpp
@@ -9,28 +9,36 @@ int main(){
     int* valptr = vals;
     
     
+//    cout << *vals << endl;
+//    cout << *valptr << endl;
+//    cout << &vals << endl;
+//    cout << valptr << endl;
+    
+    
     cout << vals << endl;
     cout << &vals << endl;
     cout << *(&vals) << endl;
     cout << *(*(&vals)) << endl; // Now it derefence to that address to give value
     cout<<valptr<<endl;
 
-    cout << endl;
-    cout << endl;
-    
-    cout<<valptr[1]<<endl;//outputs 50
-    cout<<vals[1]<<endl;//outputs 50
-    cout<<*vals<<endl;//outputs 1
-    cout<<*valptr<<endl;//outputs 1
-    
-    cout << endl;
-    cout << endl;
-    
-    cout<<*(&valptr +1)-valptr<<endl;
-
-    cout<<endl;
-
-    cout<< *(&vals + 1) -vals<<endl;
+//    cout << endl;
+//    cout << endl;
+//    
+//    cout<<valptr[1]<<endl;//outputs 50
+//    cout<<vals[1]<<endl;//outputs 50
+//    cout<<*vals<<endl;//outputs 1
+//    cout<<*valptr<<endl;//outputs 1
+//    
+//    cout << endl;
+//    cout << endl;
+//    
+//    cout<<*(&valptr +1)<<endl;
+//
+//    cout<<*(&valptr +1)-valptr<<endl;
+//
+//    cout<<endl;
+//
+//    cout<< *(&vals + 1) -vals<<endl;
 
     return 0;
 }
