d070d4c1a6bee47fb0ae1882376f3b5c9d4c24bd
chshahid119 <shahidchaudhary0729@gmail.com>
2025-06-30 19:09:28 +0100

    debugger more code added

--- C++ NOTES/DR FRANK MITROPOLOUS COURSE NOTES/COURSE NOTES (OWN)/Section12/Debugger/main.cpp
+++ C++ NOTES/DR FRANK MITROPOLOUS COURSE NOTES/COURSE NOTES (OWN)/Section12/Debugger/main.cpp
@@ -23,11 +23,15 @@ int main(){
     
     
     int x{100}, y{200};
-    cout << "\nx: " << x << endl;
-    cout << "y: " << y << endl;
+    cout << "\n x: " << x << endl;
+    cout << "\n y: " << y <<  endl;
     
     
+    swap(&x, &y);
     
+    
+    cout << "\n x: " << x << endl;
+    cout << "\n y: " << y <<  endl;
     cout << endl;
     return 0;
 }
