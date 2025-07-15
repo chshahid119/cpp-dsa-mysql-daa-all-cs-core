61ffc3e8e1e708409298fe35148f40bbd047b81a
chshahid119 <shahidchaudhary0729@gmail.com>
2025-07-15 01:42:41 +0100

    Add Constructor Initlization small example

--- C++ NOTES/DR FRANK MITROPOLOUS COURSE NOTES/COURSE NOTES (OWN)/Section 13/ConstructorsInitilizationLists/main.cpp
+++ C++ NOTES/DR FRANK MITROPOLOUS COURSE NOTES/COURSE NOTES (OWN)/Section 13/ConstructorsInitilizationLists/main.cpp
@@ -1,14 +1,34 @@
 
-
 #include <iostream>
+#include <string>
+
 
 using namespace std;
 
 class Player{
     
+private:
+    string name;
+    int age;
+    int xp;
+
+public:
+//Constructor 
+Player (string n, int a, int x) 
+: name{n}, age{a}, xp{x} {
+    cout << "Parameterized Constructor" << endl;
+}
+
+
+~Player(){
+    cout << "Player Destructor Called" << endl;
+}
+    
 };
 
 int main(){
+    
+    Player obj1 {"Shahid", 19, 6};
     
     
     return 0;
